f7f31aa457e9468e63f77297681738550f081989
PAKSAMEM\syami <syamimsaifuddin@outlook.com>
2024-03-21 21:16:54 +0800

    no message

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/users", (req, res) => res.send("hello users"));
