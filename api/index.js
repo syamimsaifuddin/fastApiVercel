d7d76dd46f5603ee0e78d97a5f30144202f9a296
PAKSAMEM\syami <syamimsaifuddin@outlook.com>
2024-03-21 20:57:24 +0800

    no message

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-import app from '../app'
+// import app from '../app'
 
-export default app
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/users", (req, res) => res.send("hello users"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
