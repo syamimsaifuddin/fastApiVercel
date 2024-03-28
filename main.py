023b7a1951fd2b53dbd07962aa130b66f85ce81b
PAKSAMEM\syami <syamimsaifuddin@outlook.com>
2024-03-28 22:44:28 +0800

    no message

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from app import app
+from os import getenv
+import uvicorn
 
 if __name__ == "__main__":
-    app.run(debugs=True)
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("main:app", port = port, reload = True)
+    # app.run(debugs=True) # for testing
